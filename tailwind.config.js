f5e7672cbe56d82d897b17bca11c3a9035abded1
Rakib <hellorakibhossen@gmail.com>
2024-08-23 06:56:21 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      Garamond: ["EB Garamond", system - ui],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
